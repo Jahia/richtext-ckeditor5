69c58f78da85d91169e398080201d7652c246fb2
Thomas Draier <tdraier@jahia.com>
2023-01-20 13:56:37 +0100

    Inline edit save

--- src/javascript/RichTextCKEditor5/InlineCKEditor5.jsx
+++ src/javascript/RichTextCKEditor5/InlineCKEditor5.jsx
@@ -2,8 +2,23 @@ import {JahiaBalloonEditor} from '~/CKEditor/JahiaBalloonEditor';
 
 export const inlineEdit = (element, saveCallback) => {
     console.log('editing element', element, saveCallback);
-    JahiaBalloonEditor.create(element, {
-    }).then(editor => {
+    const dirty = {
+        current: false
+    };
+    JahiaBalloonEditor.create(element, {}).then(editor => {
+        editor.model.document.on('change:data', () => {
+            console.log('The data has changed!');
+            dirty.current = true;
+        });
+
+        editor.ui.focusTracker.on('change:isFocused', (evt, name, isFocused) => {
+            if (!isFocused && dirty.current) {
+                console.log('ft blur', evt, name, isFocused);
+                saveCallback(editor.getData());
+                dirty.current = false;
+            }
+        });
+
         console.log('Editor was initialized', editor);
     }).catch(err => {
         console.error(err.stack);

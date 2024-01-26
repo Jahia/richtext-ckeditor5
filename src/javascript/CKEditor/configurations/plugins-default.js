b42c151c7052bf1d6716f8eee10f70829009ff0b
Geofrey Flores <75278792+gflores-jahia@users.noreply.github.com>
2024-01-26 14:06:38 -0500

    BACKLOG-21981: Refactor default CK5 configuration (#36)
    * Refactor default CK5 configuration
    
    * fix lint

---
+++ src/javascript/CKEditor/configurations/plugins-default.js
@@ -0,0 +1,106 @@
+import {Alignment} from '@ckeditor/ckeditor5-alignment';
+import {Autoformat} from '@ckeditor/ckeditor5-autoformat';
+import {BlockQuote} from '@ckeditor/ckeditor5-block-quote';
+import {Bold, Code, Italic, Strikethrough, Underline} from '@ckeditor/ckeditor5-basic-styles';
+import {CKBox} from '@ckeditor/ckeditor5-ckbox';
+import {CloudServices} from '@ckeditor/ckeditor5-cloud-services';
+import {DocumentList, DocumentListProperties} from '@ckeditor/ckeditor5-list';
+import {Essentials} from '@ckeditor/ckeditor5-essentials';
+import {ExportPdf} from '@ckeditor/ckeditor5-export-pdf';
+import {FindAndReplace} from '@ckeditor/ckeditor5-find-and-replace';
+import {FontBackgroundColor, FontColor, FontFamily, FontSize} from '@ckeditor/ckeditor5-font';
+import {GeneralHtmlSupport} from '@ckeditor/ckeditor5-html-support';
+import {Heading} from '@ckeditor/ckeditor5-heading';
+import {HorizontalLine} from '@ckeditor/ckeditor5-horizontal-line';
+import {HtmlEmbed} from '@ckeditor/ckeditor5-html-embed';
+import {Image, ImageCaption, ImageResize, ImageStyle, ImageToolbar, ImageUpload, PictureEditing} from '@ckeditor/ckeditor5-image';
+import {Indent, IndentBlock} from '@ckeditor/ckeditor5-indent';
+import {Link} from '@ckeditor/ckeditor5-link';
+import {List} from '@ckeditor/ckeditor5-list';
+import {MediaEmbed} from '@ckeditor/ckeditor5-media-embed';
+import {Paragraph} from '@ckeditor/ckeditor5-paragraph';
+import {PasteFromOffice} from '@ckeditor/ckeditor5-paste-from-office';
+import {RemoveFormat} from '@ckeditor/ckeditor5-remove-format';
+import {SourceEditing} from '@ckeditor/ckeditor5-source-editing';
+import {SpecialCharacters, SpecialCharactersEssentials} from '@ckeditor/ckeditor5-special-characters';
+import {Style} from '@ckeditor/ckeditor5-style';
+import {Table, TableCellProperties, TableColumnResize, TableProperties, TableToolbar} from '@ckeditor/ckeditor5-table';
+import {TextTransformation} from '@ckeditor/ckeditor5-typing';
+import {Picker} from '~/CKEditor/Picker/Picker';
+
+export const plugins = {
+    classic: [
+        Alignment,
+        Autoformat,
+        BlockQuote,
+        Bold,
+        CloudServices,
+        Code,
+        DocumentList,
+        DocumentListProperties,
+        Essentials,
+        ExportPdf,
+        FindAndReplace,
+        FontBackgroundColor,
+        FontColor,
+        FontFamily,
+        FontSize,
+        GeneralHtmlSupport,
+        Heading,
+        HorizontalLine,
+        HtmlEmbed,
+        Image,
+        ImageCaption,
+        ImageResize,
+        ImageStyle,
+        ImageToolbar,
+        ImageUpload,
+        Indent,
+        IndentBlock,
+        Italic,
+        Link,
+        MediaEmbed,
+        Paragraph,
+        PasteFromOffice,
+        RemoveFormat,
+        SourceEditing,
+        SpecialCharacters,
+        SpecialCharactersEssentials,
+        Strikethrough,
+        Style,
+        Table,
+        TableCellProperties,
+        TableColumnResize,
+        TableProperties,
+        TableToolbar,
+        TextTransformation,
+        Underline,
+        Picker
+    ],
+    balloon: [
+        Autoformat,
+        BlockQuote,
+        Bold,
+        CKBox,
+        CloudServices,
+        Essentials,
+        GeneralHtmlSupport,
+        Heading,
+        Image,
+        ImageCaption,
+        ImageStyle,
+        ImageToolbar,
+        ImageUpload,
+        Indent,
+        Italic,
+        Link,
+        List,
+        MediaEmbed,
+        Paragraph,
+        PasteFromOffice,
+        PictureEditing,
+        Table,
+        TableToolbar,
+        TextTransformation
+    ]
+};
---
+++ src/javascript/RichTextCKEditor5.constants.js
@@ -0,0 +1,10 @@
+const Constants = {
+    registry: {
+        MODULE_KEY: 'richtext-ckeditor5',
+        CONFIG_KEY: 'ckeditor5-config',
+        PLUGINS_KEY: 'ckeditor5-plugins',
+        BALLOON_PLUGINS_KEY: 'ckeditor5-balloon-plugins'
+    }
+};
+
+export default Constants;

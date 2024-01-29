7eb7e63c2a98ac3978eea4d8a90cd7be5cbf4c4e
akarmanov <alexandrk_@hotmail.com>
2024-01-29 12:35:06 -0500

    BACKLOG-21980 Add hooks to swap and restore selector

---
+++ src/javascript/RichTextCKEditor5/editorHooks.js
@@ -0,0 +1,33 @@
+import {getEditorVersionInfo} from "./RichTextCKEditor5.gql-queries";
+import {registry} from "@jahia/ui-extender";
+
+let originalRichText;
+
+export const editorOnBeforeContextHook = async (editContext, client) => {
+    if (!originalRichText) {
+        originalRichText = registry.get('selectorType', 'RichText')
+    }
+
+    return new Promise((resolve, reject) => {
+        client.query({
+            query: getEditorVersionInfo,
+            variables: {siteId: editContext.siteInfo.uuid}
+        }).then(result => {
+            if (!result.data.jcr.nodeById?.property?.booleanValue) {
+                registry.addOrReplace('selectorType', 'RichText', registry.get('selectorType', 'RichText5'));
+            } else {
+                registry.addOrReplace('selectorType', 'RichText', originalRichText);
+            }
+            resolve()
+        }).catch( e => {
+            console.error(e);
+            reject();
+        })
+    });
+}
+
+export const editorOnCloseHook = () => {
+    if (originalRichText) {
+        registry.addOrReplace('selectorType', 'RichText', originalRichText);
+    }
+}
--- src/javascript/RichTextCKEditor5/registerRichTextCKEditor5.js
+++ src/javascript/RichTextCKEditor5/registerRichTextCKEditor5.js
@@ -1,8 +1,11 @@
 import {RichTextCKEditor5} from './RichTextCKEditor5';
 import {registry} from '@jahia/ui-extender';
 import {inlineEdit} from '~/RichTextCKEditor5/InlineCKEditor5';
+import {editorOnBeforeContextHook, editorOnCloseHook} from './editorHooks';
 
 export const registerRichTextCKEditor5 = () => {
     registry.addOrReplace('selectorType', 'RichText5', {cmp: RichTextCKEditor5, supportMultiple: false});
     registry.addOrReplace('inline-editor', 'ckeditor5', {callback: inlineEdit});
+    registry.addOrReplace('jcontent-editor-onbefore-context-hook', 'richtext-ckeditor5', {hook: editorOnBeforeContextHook});
+    registry.addOrReplace('jcontent-editor-onclose-hook', 'richtext-ckeditor5', {hook: editorOnCloseHook});
 };

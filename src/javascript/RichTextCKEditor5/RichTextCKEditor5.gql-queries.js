0da053d7ec4e27067fcdb8dd6c9ff67a6eb1dfc3
Alex Karmanov <alexandrk_@hotmail.com>
2024-01-29 15:26:08 -0500

    BACKLOG-21980 Swap richtext editor depending on version used (#38)
    * BACKLOG-21980 Add mixin to mark sites with ckeditor4
    
    * BACKLOG-21980 Add groovy to patches
    
    * BACKLOG-21980 Add hooks to swap and restore selector
    
    * BACKLOG-21980 Put resource bundle in subfolder
    
    * BACKLOG-21980 Fix lint
    
    * BACKLOG-21980 Remove warning

--- src/javascript/RichTextCKEditor5/RichTextCKEditor5.gql-queries.js
+++ src/javascript/RichTextCKEditor5/RichTextCKEditor5.gql-queries.js
@@ -1,4 +1,5 @@
 import gql from 'graphql-tag';
+import {PredefinedFragments} from '@jahia/data-helper';
 
 export const getCKEditorConfigurationPath = gql`
     query getCKEditorConfigurationPath($nodePath: String!) {
@@ -7,4 +8,18 @@ export const getCKEditorConfigurationPath = gql`
             ckeditorToolbar(nodePath: $nodePath)
         }
     }
+`;
+
+export const getEditorVersionInfo = gql`
+    query getEditorVersionInfo($siteId: String!) {
+      jcr {
+        nodeById(uuid: $siteId) {
+          property(name: "useCkEditor4") {
+            booleanValue
+          }
+          ...NodeCacheRequiredFields
+        }
+      }
+    }
+    ${PredefinedFragments.nodeCacheRequiredFields.gql}
 `;
---
+++ src/javascript/RichTextCKEditor5/editorHooks.js
@@ -0,0 +1,34 @@
+import {getEditorVersionInfo} from './RichTextCKEditor5.gql-queries';
+import {registry} from '@jahia/ui-extender';
+
+let originalRichText;
+
+export const editorOnBeforeContextHook = async (editContext, client) => {
+    if (!originalRichText) {
+        originalRichText = registry.get('selectorType', 'RichText');
+    }
+
+    return new Promise((resolve, reject) => {
+        client.query({
+            query: getEditorVersionInfo,
+            variables: {siteId: editContext.siteInfo.uuid}
+        }).then(result => {
+            if (result.data.jcr.nodeById?.property?.booleanValue) {
+                registry.addOrReplace('selectorType', 'RichText', originalRichText);
+            } else {
+                registry.addOrReplace('selectorType', 'RichText', registry.get('selectorType', 'RichText5'));
+            }
+
+            resolve();
+        }).catch(e => {
+            console.error(e);
+            reject();
+        });
+    });
+};
+
+export const editorOnCloseHook = () => {
+    if (originalRichText) {
+        registry.addOrReplace('selectorType', 'RichText', originalRichText);
+    }
+};

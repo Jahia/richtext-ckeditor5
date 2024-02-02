bcc824156dba480920507ba960f526cd2230fab4
Cedric Mailleux <cmailleux@jahia.com>
2024-02-02 13:56:05 -0500

    BACKLOG-22300: Validate type of picker opened

--- tests/cypress/e2e/siteConfig.cy.ts
+++ tests/cypress/e2e/siteConfig.cy.ts
@@ -1,4 +1,4 @@
-import {JContent, ContentEditor} from '@jahia/jcontent-cypress/dist/page-object';
+import {JContent, ContentEditor, } from '@jahia/jcontent-cypress/dist/page-object';
 import {createSite, deleteSite} from '@jahia/cypress';
 import {Ckeditor5} from '../page-object/ckeditor5';
 
@@ -83,5 +83,33 @@ describe('Rich Text CKeditor 5 - Site level configuration tests', () => {
         ckeditor5.getRichTextCKeditor5Field('jnt:bigText_text').selectHeading('Heading 2');
         ckeditor5.getRichTextCKeditor5Field('jnt:bigText_text').getData().should('eq', `<h2>${newlyCreatedContentCKEditor5}</h2>`);
         ckeditor5.save();
+    });
+
+    it('Opens image picker when insert an image', function () {
+        jcontent = visitContentFolders(siteKey, jcontent);
+        cy.apollo({
+            mutationFile: 'toggleCkeditorVersionOnSite.graphql',
+            variables: {sitePath: `/sites/${siteKey}`, ckeditor4: 'false'}
+        });
+        jcontent.createContent('Rich text');
+        const ckeditor5 = new Ckeditor5();
+        ckeditor5.getRichTextCKeditor5Field('jnt:bigText_text').getToolbarButton('Insert image').click();
+        cy.get('div[data-sel-role="picker-dialog"][data-sel-type="image"]').should('be.visible');
+        cy.get('button[data-sel-picker-dialog-action="cancel"]').click();
+        ckeditor5.cancel();
+    });
+
+    it('Opens link picker when insert a link', function () {
+        jcontent = visitContentFolders(siteKey, jcontent);
+        cy.apollo({
+            mutationFile: 'toggleCkeditorVersionOnSite.graphql',
+            variables: {sitePath: `/sites/${siteKey}`, ckeditor4: 'false'}
+        });
+        jcontent.createContent('Rich text');
+        const ckeditor5 = new Ckeditor5();
+        ckeditor5.getRichTextCKeditor5Field('jnt:bigText_text').getToolbarButton('Insert link').click();
+        cy.get('div[data-sel-role="picker-dialog"][data-sel-type="editoriallink"]').should('be.visible');
+        cy.get('button[data-sel-picker-dialog-action="cancel"]').click();
+        ckeditor5.cancel();
     });
 });

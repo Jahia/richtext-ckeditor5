a0005d453e5d34e518ce625917e2ef7a9caa5488
Geofrey Flores <gflores@jahia.com>
2024-03-26 16:10:42 -0400

    BACKLOG-22140: Switch to classic editor

--- src/javascript/CKEditor/JahiaClassicEditor.js
+++ src/javascript/CKEditor/JahiaClassicEditor.js
@@ -2,10 +2,10 @@
  * @license Copyright (c) 2014-2022, CKSource Holding sp. z o.o. All rights reserved.
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
-import {DecoupledEditor} from '@ckeditor/ckeditor5-editor-decoupled';
+import {ClassicEditor} from '@ckeditor/ckeditor5-editor-classic';
 import {isElement} from 'lodash-es';
 
-export class JahiaClassicEditor extends DecoupledEditor {
+export class JahiaClassicEditor extends ClassicEditor {
     static create(sourceElementOrData, config = {}) {
         const editor = new this(sourceElementOrData, config);
         return editor.initPlugins().then(() => {

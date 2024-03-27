2d2c793abf73ce3f965a7a710f57169b3e94bae3
Geofrey Flores <75278792+gflores-jahia@users.noreply.github.com>
2024-03-27 13:25:42 -0400

    BACKLOG-22531: Remove hardcoded template stylings (#54)

--- src/javascript/RichTextCKEditor5/RichTextCKEditor5.jsx
+++ src/javascript/RichTextCKEditor5/RichTextCKEditor5.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useEffect, useRef} from 'react';
 import * as PropTypes from 'prop-types';
 import {CKEditor} from '@ckeditor/ckeditor5-react';
 import {JahiaClassicEditor} from '../CKEditor/JahiaClassicEditor';
@@ -7,7 +7,6 @@ import {useContentEditorConfigContext, useContentEditorContext} from '@jahia/jco
 import {useApolloClient, useQuery} from '@apollo/client';
 import {getCKEditorConfigurationPath} from '~/RichTextCKEditor5/RichTextCKEditor5.gql-queries';
 import {useStore} from 'react-redux';
-import scopeCss from 'scope-css';
 import {set} from '~/RichTextCKEditor5/RichTextCKEditor5.utils';
 
 export const RichTextCKEditor5 = ({field, id, value, onChange, onBlur}) => {
@@ -15,7 +14,6 @@ export const RichTextCKEditor5 = ({field, id, value, onChange, onBlur}) => {
     const client = useApolloClient();
     const store = useStore();
     const {lang, uilang} = useContentEditorConfigContext();
-    const [style, setStyle] = useState();
     const toolbarContainer = useRef();
 
     useEffect(() => {
@@ -27,29 +25,6 @@ export const RichTextCKEditor5 = ({field, id, value, onChange, onBlur}) => {
             }
         }
     }, [value]);
-
-    // eslint-disable-next-line no-warning-comments
-    // Todo : load from config
-    const contentCss = '/modules/dx-base-demo-templates/css/app.css';
-
-    useEffect(() => {
-        fetch(contentCss).then(css => css.text()).then(css => {
-            const scoped = scopeCss(css, '.ck-content');
-            const style = document.createElement('style');
-            style.textContent = scoped;
-            setStyle(style);
-        });
-    }, [contentCss]);
-
-    useEffect(() => {
-        if (style) {
-            document.head.append(style);
-
-            return () => {
-                style.remove();
-            };
-        }
-    }, [style]);
 
     const parsedOptions = {};
     field.selectorOptions.forEach(option => {

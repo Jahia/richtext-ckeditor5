f2daa0d21690331b607d3703caf8e42d9b5c3493
akarmanov <alexandrk_@hotmail.com>
2024-04-02 12:43:03 -0400

    BACKLOG-22141 Update licence test

--- tests/cypress/e2e/licence.cy.ts
+++ tests/cypress/e2e/licence.cy.ts
@@ -19,13 +19,13 @@ describe('Licence tests', () => {
         jcontent = JContent.visit(siteKey, 'en', 'content-folders/contents');
     });
 
-    it('should have licenseKey set to community', () => {
+    it('should have licenseKey set to enterprise', () => {
         jcontent.createContent('Rich text');
         cy.window().then(win => {
             // eslint-disable-next-line @typescript-eslint/ban-ts-comment
             // @ts-ignore
             const config = win.jahia.uiExtender.registry.get('@jahia/ckeditor5', 'shared').getDefaultConfig();
-            expect(config.licenseKey).to.equal('CKEDITOR_COMMUNITY');
+            expect(config.licenseKey).to.have.length(100);
         });
     });
 });

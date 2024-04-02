875fcd2e7a4d1415e2bd991b7d4c6a197f7636e1
akarmanov <alexandrk_@hotmail.com>
2024-04-02 10:56:22 -0400

    BACKLOG-22141 Print env

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,7 @@ require('dotenv').config({ path: './.env' });
 module.exports = (env, argv) => {
     let _argv = argv || {};
 
+    console.log(process.env);
     let config = {
         entry: {
             main: path.resolve(__dirname, 'src/javascript/index')

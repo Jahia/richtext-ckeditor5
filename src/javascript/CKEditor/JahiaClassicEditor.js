37b846ec7f3f85db14864a8242b15dc8e4f5873e
Alex Karmanov <alexandrk_@hotmail.com>
2024-04-03 10:29:12 -0400

    BACKLOG-22141 Move config update to the editor class (#57)
    * BACKLOG-22141 Move config update to the editor class
    
    * BACKLOG-22141 Fix lint

--- src/javascript/CKEditor/JahiaClassicEditor.js
+++ src/javascript/CKEditor/JahiaClassicEditor.js
@@ -5,8 +5,29 @@
 import {ClassicEditor} from '@ckeditor/ckeditor5-editor-classic';
 import {isElement} from 'lodash-es';
 
+const productivityPluginsNameAndKey = {
+    FormatPainter: true,
+    formatPainter: true,
+    ExportPdf: true,
+    exportPdf: true,
+    ExportWord: true,
+    exportWord: true
+};
+
 export class JahiaClassicEditor extends ClassicEditor {
     static create(sourceElementOrData, config = {}) {
+        config = {...JahiaClassicEditor.defaultConfig, ...config};
+
+        // eslint-disable-next-line
+        const isProductivityEnabled = window?.contextJsParameters?.valid && CKEDITOR_PRODUCTIVITY_LICENSE;
+        // eslint-disable-next-line
+        config.licenseKey = isProductivityEnabled ? CKEDITOR_PRODUCTIVITY_LICENSE : 'CKEDITOR_COMMUNITY';
+
+        if (!isProductivityEnabled) {
+            JahiaClassicEditor.builtinPlugins = JahiaClassicEditor.builtinPlugins.filter(p => !productivityPluginsNameAndKey[p.pluginName]);
+            config.toolbar.items = config.toolbar.items.filter(i => !productivityPluginsNameAndKey[i]);
+        }
+
         const editor = new this(sourceElementOrData, config);
         return editor.initPlugins().then(() => {
             editor.ui.init(isElement(sourceElementOrData) ? sourceElementOrData : null);

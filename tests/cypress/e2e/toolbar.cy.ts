b3b9c0b12ea77a971b22b3b213a69f157f599a52
akarmanov <alexandrk_@hotmail.com>
2024-04-25 10:32:43 -0400

    BACKLOG-22631 Test toolbar buttons to be present and working

--- tests/cypress/e2e/toolbar.cy.ts
+++ tests/cypress/e2e/toolbar.cy.ts
@@ -20,6 +20,18 @@ describe('Toolbar tests', () => {
         jcontent = JContent.visit(siteKey, 'en', 'content-folders/contents');
     });
 
+    it('should have buttons visible and clickable', () => {
+        jcontent.createContent('Rich text');
+        const ckeditor5 = new Ckeditor5();
+        const ck5field: RichTextCKeditor5Field = ckeditor5.getRichTextCKeditor5Field('jnt:bigText_text');
+        ck5field.type('this is my text');
+        ck5field.getToolbarButton('Bold (⌘B)').click();
+        ck5field.getToolbarButton('Italic (⌘I)').click();
+        ck5field.getToolbarButton('Strikethrough (⌘⇧X)').click();
+        ck5field.getToolbarButton('Heading').click().click();
+        ck5field.getToolbarButton('Text alignment').click().click();
+    });
+
     // To be reenabled once source saving is fixed properly
     it.skip('should have source button working', () => {
         jcontent.createContent('Rich text');

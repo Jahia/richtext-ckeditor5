928af7cb3aaba99da612514c330e86b174cab1c4
Alex Karmanov <alexandrk_@hotmail.com>
2024-04-25 14:38:29 -0400

    BACKLOG-22631 Test toolbar buttons to be present and working (#67)
    * BACKLOG-22631 Test toolbar buttons to be present and working
    
    * BACKLOG-22631 Add test for jahia picker buttons
    
    * BACKLOG-22631 Fix lint
    
    * BACKLOG-22631 Remove key shortcut hints

--- tests/cypress/e2e/toolbar.cy.ts
+++ tests/cypress/e2e/toolbar.cy.ts
@@ -1,5 +1,6 @@
 import {JContent} from '@jahia/jcontent-cypress/dist/page-object';
-import {createSite, deleteSite} from '@jahia/cypress';
+import {Picker} from '@jahia/jcontent-cypress/dist/page-object/picker';
+import {createSite, deleteSite, getComponentByRole} from '@jahia/cypress';
 import {Ckeditor5, RichTextCKeditor5Field} from '../page-object/ckeditor5';
 
 describe('Toolbar tests', () => {
@@ -18,6 +19,24 @@ describe('Toolbar tests', () => {
     beforeEach(() => {
         cy.loginAndStoreSession();
         jcontent = JContent.visit(siteKey, 'en', 'content-folders/contents');
+    });
+
+    it('should have buttons visible and clickable', () => {
+        jcontent.createContent('Rich text');
+        const ckeditor5 = new Ckeditor5();
+        const ck5field: RichTextCKeditor5Field = ckeditor5.getRichTextCKeditor5Field('jnt:bigText_text');
+        ck5field.type('this is my text');
+        ck5field.getToolbarButton('Bold').click();
+        ck5field.getToolbarButton('Italic').click();
+        ck5field.getToolbarButton('Remove Format').click();
+        ck5field.getToolbarButton('Heading').click().click();
+        ck5field.getToolbarButton('Text alignment').click().click();
+        ck5field.getToolbarButton('Insert image or file').click();
+        let picker: Picker = getComponentByRole(Picker, 'picker-dialog');
+        picker.cancel();
+        ck5field.getToolbarButton('Insert link').click();
+        picker = getComponentByRole(Picker, 'picker-dialog');
+        picker.cancel();
     });
 
     // To be reenabled once source saving is fixed properly

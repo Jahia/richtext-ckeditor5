57f3fcf7682e14e3be29d7fa6b94ad809816f8b5
Geofrey Flores <gflores@jahia.com>
2025-04-22 16:03:23 -0400

    Use isPremiumPlugin flag for filtering premium plugins

--- src/javascript/CKEditor/JahiaClassicEditor.js
+++ src/javascript/CKEditor/JahiaClassicEditor.js
@@ -8,26 +8,16 @@ import {isElement} from 'lodash-es';
 import 'ckeditor5/ckeditor5.css';
 import {isProductivityMode} from '../RichTextCKEditor5/RichTextCKEditor5.utils';
 
-const productivityPluginsNameAndKey = {
-    FormatPainter: true,
-    formatPainter: true,
-    ExportPdf: true,
-    exportPdf: true,
-    ExportWord: true,
-    exportWord: true
-};
-
 export class JahiaClassicEditor extends ClassicEditor {
     static create(sourceElementOrData, config = {}) {
         config = {...JahiaClassicEditor.defaultConfig, ...config};
 
-        const isProductivityEnabled = isProductivityMode();
-        // eslint-disable-next-line
-        config.licenseKey = isProductivityEnabled ? CKEDITOR_PRODUCTIVITY_LICENSE : 'GPL';
-
-        if (!isProductivityEnabled) {
-            JahiaClassicEditor.builtinPlugins = JahiaClassicEditor.builtinPlugins.filter(p => !productivityPluginsNameAndKey[p.pluginName]);
-            config.toolbar.items = config.toolbar.items.filter(i => !productivityPluginsNameAndKey[i]);
+        if (isProductivityMode()) {
+            config.licenseKey = CKEDITOR_PRODUCTIVITY_LICENSE;
+        } else {
+            config.licenseKey = 'GPL';
+            JahiaClassicEditor.builtinPlugins = JahiaClassicEditor.builtinPlugins
+                .filter(p => Boolean(p.isPremiumPlugin));
         }
 
         const editor = new this(sourceElementOrData, config);
--- src/javascript/RichTextCKEditor5/RichTextCKEditor5.utils.js
+++ src/javascript/RichTextCKEditor5/RichTextCKEditor5.utils.js
@@ -57,41 +57,5 @@ export const set = (target, path, value) => {
     }
 };
 
-export const isObject = item => {
-    return (item && typeof item === 'object' && !Array.isArray(item));
-};
-
-export const mergeDeep = (target, ...sources) => {
-    if (!sources.length) {
-        return target;
-    }
-
-    const source = sources.shift();
-
-    if (isObject(target) && isObject(source)) {
-        for (const key in source) {
-            if (isObject(source[key])) {
-                if (!target[key]) {
-                    Object.assign(target, {[key]: {}});
-                }
-
-                mergeDeep(target[key], source[key]);
-            } else {
-                Object.assign(target, {[key]: source[key]});
-            }
-        }
-    }
-
-    return mergeDeep(target, ...sources);
-};
-
-export const arrayValue = value => {
-    return (typeof value === 'string') ? value.split(',') : value;
-};
-
-export const booleanValue = v => typeof v === 'string' ? v === 'true' : Boolean(v);
-
-export const toArray = value => (Array.isArray(value) ? value : [value]);
-
-// eslint-disable-next-line
-export const isProductivityMode = () => window?.contextJsParameters?.valid && CKEDITOR_PRODUCTIVITY_LICENSE;
+// eslint-disable-next-line no-undef
+export const isProductivityMode = () => window?.contextJsParameters?.valid && Boolean(CKEDITOR_PRODUCTIVITY_LICENSE);

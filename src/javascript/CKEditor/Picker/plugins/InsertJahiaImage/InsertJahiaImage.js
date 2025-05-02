ae3d6e8f682ea8db48d2434ecbf561b5681d35be
akarmanov <alexandrk_@hotmail.com>
2025-05-02 15:05:06 -0400

    fix: Translation experiment

--- src/javascript/CKEditor/Picker/plugins/InsertJahiaImage/InsertJahiaImage.js
+++ src/javascript/CKEditor/Picker/plugins/InsertJahiaImage/InsertJahiaImage.js
@@ -4,6 +4,7 @@ import {
     Plugin
 } from 'ckeditor5';
 import {JahiaInsertImageCommand} from '../../JahiaInsertImageCommand';
+import {loadTranslations} from '../../../../RichTextCKEditor5/RichTextCKEditor5.utils';
 
 // This is taken from moonstone as I did not find a way to import and use "react" component directly or import pure svg
 const imageIcon = '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M21 3H3C2 3 1 4 1 5V19C1 20.1 1.9 21 3 21H21C22 21 23 20 23 19V5C23 4 22 3 21 3ZM5 17L8.5 12.5L11 15.51L14.5 11L19 17H5Z" fill="black"/></svg>';
@@ -17,8 +18,36 @@ export class InsertJahiaImage extends Plugin {
         return [Notification, ImageEditing, LinkEditing];
     }
 
+    static get requiresTranslations() {
+        return true;
+    }
+
+    static prefixedTranslationKey(key) {
+        return `${InsertJahiaImage.pluginName}:${key}`;
+    }
+
+    static get translations() {
+        return {
+            fr: {
+                [InsertJahiaImage.prefixedTranslationKey('label')]: 'fr Image',
+                [InsertJahiaImage.prefixedTranslationKey('tooltip')]: 'fr insert image'
+            },
+            de: {
+                [InsertJahiaImage.prefixedTranslationKey('label')]: 'de Image',
+                [InsertJahiaImage.prefixedTranslationKey('tooltip')]: 'de insert image'
+            },
+            en: {
+                [InsertJahiaImage.prefixedTranslationKey('label')]: 'Image',
+                [InsertJahiaImage.prefixedTranslationKey('tooltip')]: 'insert image'
+            }
+        };
+    }
+
     init() {
         const editor = this.editor;
+        const t = editor.t;
+        const ts = InsertJahiaImage.translations;
+        loadTranslations(editor, ts);
 
         editor.commands.add('jahiaInsertImageCommand', new JahiaInsertImageCommand(editor, 'image'));
 
@@ -27,8 +56,8 @@ export class InsertJahiaImage extends Plugin {
             const view = new MenuBarMenuListItemButtonView(locale);
 
             view.set({
-                label: 'Image',
-                tooltip: 'Insert image',
+                label: t(InsertJahiaImage.prefixedTranslationKey('label')),
+                tooltip: t(InsertJahiaImage.prefixedTranslationKey('tooltip')),
                 withText: true,
                 icon: imageIcon
             });
@@ -44,8 +73,8 @@ export class InsertJahiaImage extends Plugin {
             const view = new ButtonView(locale);
 
             view.set({
-                label: 'Image',
-                tooltip: 'Insert image',
+                label: t(InsertJahiaImage.prefixedTranslationKey('label')),
+                tooltip: t(InsertJahiaImage.prefixedTranslationKey('tooltip')),
                 withText: false,
                 icon: imageIcon
             });

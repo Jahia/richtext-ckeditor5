3f08b34e4987ac6973575fc373ebd4a4a41182cd
Geofrey Flores <75278792+gflores-jahia@users.noreply.github.com>
2026-02-18 08:24:27 -0500

    chore: Adjust dev SNAPSHOT version for test module (#269)
    * chore: Adjust dev SNAPSHOT version for test module
    
    * fix on-code-change test build step
    
    * fix image test

--- tests/cypress/e2e/image.cy.ts
+++ tests/cypress/e2e/image.cy.ts
@@ -89,7 +89,7 @@ describe('Image tests', () => {
             .should('contain', linkUrl);
     });
 
-    it('should be able to resize image and display in page builder', () => {
+    it('should be able to resize image and display in page builder', {retries: 3}, () => {
         const jcontent = JContent.visit(siteKey, 'en', 'pages/home')
             .switchToListMode();
         const ce = jcontent.editComponentByText('Lorem ipsum');
